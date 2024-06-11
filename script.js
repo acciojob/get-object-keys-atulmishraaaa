b013e155a4739c12a43bd3745aa5e7a265f34b9b
Atul Mishra <139470533+atulmishraaaa@users.noreply.github.com>
2024-06-11 13:46:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-//your JS code here. If required.
+// Creating the student object
 const student = {
-	name: "Atul";
-}
-object.prototype.getKeys = function() {
-	return object.keys(this);
-	
-}
-console.log(student.getKeys());
+  name: "John"
+};
+
+// Adding getKeys() method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Testing the getKeys() method
+console.log(student.getKeys()); // Output: ["name"]
